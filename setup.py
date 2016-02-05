b46295060f0afe6340ebdfa7f07412be5b4e62ca
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-02-05 18:01:47 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!./env/bin/python
+
+from setuptools import setup
+
+setup(
+    name='pyutil',
+    version='v0.1.0',
+    packages=["pyutil"],
+    author='Lobnek Wealth Management',
+    author_email='thomas.schmelzer@lobnek.com',
+    description='', install_requires=['requests>=2.8.1', 'pandas>=0.17.0']
+)
