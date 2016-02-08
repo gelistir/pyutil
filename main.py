a34bbe4749cec7f9817e68fac26f09cd1f2dd02b
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-02-08 18:07:19 +0100

    initial commit, log for Py2 and Py3

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from pyutil.log import get_logger, StreamHandler, MailHandler
 from pyutil.message import Mail
 
 if __name__ == '__main__':
-
     # this is one way to get the data needed to hookup with the restful API of Mailgun.
     # Obviously we have to protect our configuration file. So this code will fail on your local machine
     # unless you have the file lobnek.cfg in your homepath
-    config = Configuration(file = join(expanduser("~"), "lobnek.cfg"))
+    config = Configuration(file=join(expanduser("~"), "lobnek.cfg"))
     __api = config["Mailgun"]["mailgunapi"]
     __key = config["Mailgun"]["mailgunkey"]
 
@@ -20,4 +19,3 @@ if __name__ == '__main__':
     logger.addHandler(MailHandler(m))
     logger.addHandler(StreamHandler())
     logger.error("I shot the sheriff.")
-
