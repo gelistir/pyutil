491ed9d8562238f2da4e93c0aaab24b6c58d4eb2
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-02-09 15:26:20 +0100

    corrected setup for v0.1.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!./env/bin/python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='v0.1.1',
-    packages=["pyutil"],
+    version='v0.1.2',
+    packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
     description='', install_requires=['requests>=2.8.1','pandas>=0.17.0']
