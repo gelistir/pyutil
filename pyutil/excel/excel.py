beec52e2128430d8f2c331327ff8642a097ce3ed
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-03-03 18:29:26 +0100

    v0.3.1 Excel sheets created in memory

--- pyutil/excel/excel.py
+++ pyutil/excel/excel.py
@@ -1,5 +1,11 @@
+# Safe import for either Python 2.x or 3.x
+try:
+    from io import BytesIO
+except ImportError:
+    from cStringIO import StringIO as BytesIO
+
 import pandas as pd
-import tempfile
+from pyutil.config import mail
 
 
 class Excel(object):
@@ -7,8 +13,8 @@ class Excel(object):
     Interaction with Excel
     """
     def __init__(self, date_format='dd/mm'):
-        self.__tmpData = tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False, prefix="lwm_")
-        self.__writer = pd.ExcelWriter(self.__tmpData.name, date_format=date_format)
+        self.__bio = BytesIO()
+        self.__writer = pd.ExcelWriter(self.__bio, engine='xlsxwriter', date_format=date_format)
         self.__format_percent = self.add_format({'num_format': '#.##%'})
 
     @property
@@ -26,9 +32,34 @@ class Excel(object):
         frame.to_excel(self.__writer, sheet_name=sheetname)
         return self.__writer.sheets[sheetname]
 
-    def close(self):
-        return self.__writer.close()
+    def save(self):
+        self.__writer.save()
+        return self
+
+    @property
+    def stream(self):
+        self.__bio.seek(0)
+        return self.__bio.read()
 
     @property
     def format_percent(self):
         return self.__format_percent
+
+
+if __name__ == '__main__':
+
+    df = pd.DataFrame(data=[[2.0,3.0]])
+    df2 = pd.DataFrame(data=[[4.0,3.0]])
+
+    e = Excel()
+    e.add_frame(df, sheetname="sheet1")
+    e.add_frame(df2, sheetname="sheet2")
+
+    m = mail()
+    m.toAdr = "thomas.schmelzer@gmail.com"
+    m.fromAdr = "mm@lobnek.com"
+    m.attach_stream("hans.xlsx", e.save().stream)
+    m.send(text="hans wurst")
+
+    print(e.stream)
+    print(e.stream)
--- test/test_excel/test_excel.py
+++ test/test_excel/test_excel.py
@@ -9,16 +9,15 @@ class TestExcel(TestCase):
     def test_init(self):
         x = Excel()
         assert isinstance(x.book, xlsxwriter.workbook.Workbook)
-        x.close()
+        x.save()
 
     def test_name(self):
         x = Excel()
-        assert x.name
-        x.close()
+        assert x.save().stream
 
     def test_write(self):
         x = Excel()
         frame = pd.DataFrame(data=[[2.0, 3.0],[4.0, 5.0]])
         sheet = x.add_frame(frame, sheetname="test")
         sheet.set_column("A:A", 20, x.format_percent)
-        x.close()
+        x.save().stream
