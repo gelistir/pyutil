505078c3191c7dd35c903c1ecb796f5949a391e4
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-04-11 15:49:16 +0200

    reader and writer aux functions v0.5.1

--- pyutil/mongo/archive.py
+++ pyutil/mongo/archive.py
@@ -1,8 +1,17 @@
 from pymongo import MongoClient
 from pymongo.database import Database
+from pyutil.mongo.reader import ArchiveReader
+from pyutil.mongo.writer import ArchiveWriter
 
 
 def database(name, host="quantsrv", port=27017):
     client = MongoClient(host, port=port)
     return Database(client, name)
 
+
+def reader(name, logger=None, host="quantsrv", port=27017):
+    return ArchiveReader(database(name, host=host, port=port), logger)
+
+
+def writer(name, logger=None, host="quantsrv", port=27017):
+    return ArchiveWriter(database(name, host=host, port=port), logger)
