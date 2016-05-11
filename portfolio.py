194978d67da5d3cd5ab8a06175dd47cc38ee3cf0
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-11 14:16:21 +0200

    portfolio example

---
+++ portfolio.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from pyutil.portfolio.portfolio import build
+
+
+if __name__ == '__main__':
+    pd.set_option("display.width", 300)
+
+    BASEDIR = os.path.dirname(__file__)
+    file_prices = os.path.join(BASEDIR, "test", "resources", "price.csv")
+    file_weights = os.path.join(BASEDIR, "test", "resources", "weight.csv")
+
+    prices = pd.read_csv(file_prices, index_col=0, parse_dates=True)
+    weights = pd.read_csv(file_weights, index_col=0, parse_dates=True)
+
+    portfolio = build(prices, weights)
+    portfolio.plot()
+
+    print(100 * portfolio.nav.monthlytable)
+
+    plt.savefig("portfolio.png")
+    plt.show()
