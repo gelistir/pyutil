51246e808676edbd273eb4f30b8d58cc12cd150d
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-13 14:12:09 +0200

    v0.7.1 correcting the Runner

--- pyutil/strategy/Runner.py
+++ pyutil/strategy/Runner.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 from pyutil.log import get_logger
 
 
@@ -6,8 +7,14 @@ class Runner(object):
     def __init__(self, module, archive, logger=None):
         self.__logger = logger or get_logger("LOBNEK")
 
+        # see: http://stackoverflow.com/questions/37209181/converting-the-py-script-into-a-string/37209507#37209507
         module = importlib.import_module(module)
-        with open(module.__file__) as ff:
+        new_module_filename = os.path.realpath(module.__file__)
+
+        if new_module_filename.endswith(".pyc"):
+            new_module_filename = new_module_filename[:-1]
+
+        with open(new_module_filename) as ff:
             self.__source = ff.read()
 
         self.__logger.debug(self.__source)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='v0.6.4',
+    version='v0.7.1',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
