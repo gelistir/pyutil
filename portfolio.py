1c320197d518f4a67b18c2f38ee7978c41af0545
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-17 16:59:57 +0200

    remove of the build function in example

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-from pyutil.portfolio.portfolio import build
+from pyutil.portfolio.portfolio import Portfolio
 
 
 if __name__ == '__main__':
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     prices = pd.read_csv(file_prices, index_col=0, parse_dates=True)
     weights = pd.read_csv(file_weights, index_col=0, parse_dates=True)
 
-    portfolio = build(prices, weights)
+    portfolio = Portfolio(prices, weights)
     portfolio.plot()
 
     print(100 * portfolio.nav.monthlytable)
