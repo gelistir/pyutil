64b25a1091797d9aae264b16bf8ec1ed4340c4dc
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-27 17:36:07 +0200

    parsing of csv more versatile

--- test/config.py
+++ test/config.py
@@ -10,12 +10,12 @@ def __f(name):
     return os.path.join(BASE_DIR, "resources", name)
 
 
-def read_frame(name, parse_dates=True):
-    return pd.read_csv(__f(name), index_col=0, header=0, parse_dates=parse_dates)
+def read_frame(name, parse_dates=True, index_col=0):
+    return pd.read_csv(__f(name), index_col=index_col, header=0, parse_dates=parse_dates)
 
 
-def read_series(name, parse_dates=True):
-    return pd.read_csv(__f( name), index_col=0, header=None, squeeze=True, parse_dates=parse_dates)
+def read_series(name, parse_dates=True, index_col=0):
+    return pd.read_csv(__f( name), index_col=index_col, header=None, squeeze=True, parse_dates=parse_dates)
 
 
 def test_portfolio():
