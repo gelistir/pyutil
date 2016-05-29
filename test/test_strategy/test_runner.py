14ebd0958f201696448ed702494b0fdcd1dd7767
thomas <thomas.schmelzer@gmail.com>
2016-05-29 13:26:17 +0200

    lean test for strategy runner

--- test/test_strategy/test_runner.py
+++ test/test_strategy/test_runner.py
@@ -1,29 +1,21 @@
-from ming import create_datastore
-from pyutil.mongo.reader import _ArchiveReader
-from pyutil.mongo.writer import _ArchiveWriter
 from test.config import read_frame
 from unittest import TestCase
 
 
+class Archive(object):
+    def __init__(self):
+        self.__prices = read_frame("price.csv", parse_dates=True)
+
+    def history(self, items):
+        return self.__prices[items]
+
+
 class TestRunner(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.db = create_datastore("tmp")
-        cls.reader = _ArchiveReader(cls.db)
-        cls.writer = _ArchiveWriter(cls.db)
-
-        # write assets into test database. Writing is slow!
-        assets = read_frame("price.csv", parse_dates=True)
-
-        # write prices into archive
-        for asset in assets:
-            cls.writer.update_asset(asset, assets[asset])
-
     def test_run(self):
         from pyutil.strategy.Runner import Runner
         module = "test.test_strategy.strat1"
 
-        r = Runner(archive=self.reader, module=module)
+        r = Runner(archive=Archive(), module=module)
 
         self.assertEqual(r.group, "testgroup")
         self.assertEqual(r.name, "test")
