ea8ecdba1e11232845b4d4bd3820f719af87de7d
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-07-21 17:18:24 +0200

    simplifying the writer v0.9.7

--- pyutil/mongo/writer.py
+++ pyutil/mongo/writer.py
@@ -29,25 +29,16 @@ class _ArchiveWriter(_ArchiveReader):
     def update_portfolio(self, key, portfolio, group, n=10, comment=""):
         self.logger.debug("Key {0}, Group {1}".format(key, group))
 
-        # check if there exists an portfolio and if so update only the last n business days of this portfolio
-        current_portfolio = self.portfolios[key]
-
         q = {"_id": key}
 
-        if current_portfolio:
-            last = current_portfolio.index[-1]
-            self.logger.debug("Last stamp {0}".format(last))
-            offset = last - pd.offsets.BDay(n=n)
+        if key in self.portfolios.keys():
+            offset = self.portfolios[key].index[-1] - pd.offsets.BDay(n=n)
             self.logger.debug("Offset {0}".format(offset))
             portfolio = portfolio.truncate(before=offset)
 
-            r = flatten("returns", portfolio.nav.returns.ix[1:])
-            w = flatten("weight", portfolio.weights)
-            p = flatten("price", portfolio.prices)
-            self.__db.strategy.update(q, w, upsert=True)
-            self.__db.strategy.update(q, p, upsert=True)
-            self.__db.strategy.update(q, r, upsert=True)
-
+            self.__db.strategy.update(q, flatten("weight", portfolio.weights), upsert=True)
+            self.__db.strategy.update(q, flatten("price", portfolio.prices), upsert=True)
+            self.__db.strategy.update(q, flatten("returns", portfolio.nav.returns.ix[1:]), upsert=True)
         else:
             self.__db.strategy.update(q, portfolio.to_json(), upsert=True)
 
@@ -67,9 +58,7 @@ class _ArchiveWriter(_ArchiveReader):
 
         if self.read_nav(name):
             xxx = today.date() + pd.offsets.MonthBegin(n=-1)
-            print(xxx)
             yyy = n.returns.truncate(before=xxx)
-            print(yyy)
         else:
             yyy = n.returns
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='v0.9.6',
+    version='v0.9.7',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
