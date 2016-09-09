e97a78dc043f6c1644aa09fac83b0f67214e5acf
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-09-09 15:14:16 +0200

    assert for correct index in portfolio and some logging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='v1.4.9',
+    version='v1.5.0',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
