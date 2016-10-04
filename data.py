449bc3472c03abbe9e0135d0cc8f6eed70162556
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-10-04 16:13:34 +0200

    data demo updated

--- data.py
+++ data.py
@@ -1,9 +1,18 @@
+import logging
+
+from pyutil.decorators import attempt
 from pyutil.mongo.archive import reader
 
+# I use the attempt decorator here. The attempt decorator embeds the function into try/except and reports any exception
+# to the logger...:
+
+@attempt
+def f(logger, archive):
+    for key in archive.portfolios.keys():
+        logger.debug(key)
+        portfolio = archive.portfolios[key]
+        print(portfolio.nav.monthlytable)
+
+
 if __name__ == '__main__':
-    r = reader(name="production", host="quantsrv")
-    for key in r.portfolios.keys():
-        try:
-            r.portfolios[key]
-        except:
-            print(key)
+    f(logger=logging.getLogger(__name__), archive=reader(name="production", host="quantsrv"))
