caee2ec86d81e4f1cfb850b258cbf3b198854c56
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-11-23 12:13:46 +0100

    v1.8.4  CsvArchive

--- pyutil/mongo/csv.py
+++ pyutil/mongo/csv.py
@@ -5,8 +5,8 @@ class CsvArchive(Archive):
     def __init__(self):
         self.__data = dict()
 
-    def put(self, name, frame):
+    def put(self, frame, name="PX_LAST"):
         self.__data[name] = frame
 
-    def history(self, items, name, before=pd.Timestamp("2002-01-01")):
+    def history(self, items, name="PX_LAST", before=pd.Timestamp("2002-01-01")):
         return self.__data[name][items].truncate(before=before)
--- test/test_mongo/test_csv.py
+++ test/test_mongo/test_csv.py
@@ -8,13 +8,13 @@ from test.config import read_frame
 class TestCsv(TestCase):
     def test_csv(self):
         reader = CsvArchive()
-        reader.put("price", frame=read_frame("price.csv", parse_dates=True))
+        reader.put(name="price", frame=read_frame("price.csv", parse_dates=True))
         a=reader.history(["A","B","C"], name="price", before=pd.Timestamp("2014-01-01"))
         self.assertAlmostEqual(a["A"][pd.Timestamp("2014-01-01")], 1200.90, places=6)
 
     def test_series(self):
         reader = CsvArchive()
-        reader.put("price", frame=read_frame("price.csv", parse_dates=True))
+        reader.put(name="price", frame=read_frame("price.csv", parse_dates=True))
         a=reader.history_series("A", name="price", before=pd.Timestamp("2014-01-01"))
         self.assertAlmostEqual(a[pd.Timestamp("2014-01-01")], 1200.90, places=6)
 
