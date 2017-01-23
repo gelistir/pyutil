cff14dbb98c22c410cc44124af4a841da9980368
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-01-23 16:40:47 +0100

    Assets and Asset concept

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='v1.9.37',
+    version='v1.9.38',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
