1d32102b72e92bde60ef948ea319ae3eda515343
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-04-25 17:56:49 +0200

    updated tests

--- test_b.sh
+++ test_b.sh
@@ -4,7 +4,7 @@ docker build --file Dockerfile-Test --tag pyutil:latest .
 # run a mongo container
 docker run -p 27017:27017 --name mongo -d mongo:latest
 
-# run all tests
+# run all tests, seems to be slow on teamcity
 docker run --rm --link mongo \
  	-v $(pwd)/test:/pyutil/test \
  	-v $(pwd)/pyutil:/pyutil/pyutil \
