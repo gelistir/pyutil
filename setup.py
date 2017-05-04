272b0be7bcbb7aa8054cb44611f69d4bab3d7032
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-05-04 17:01:08 +0200

    mongoengine into requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['requests>=2.13.0', 'pandas>=0.19.2', 'pymongo']
+    description='', install_requires=['requests>=2.13.0', 'pandas>=0.19.2', 'pymongo', 'mongoengine']
 )
