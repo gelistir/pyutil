771e882e59b1f84ce6598bfe6a063835f0d24ddc
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-05-05 10:06:08 +0200

    test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-docker build --file Dockerfile-Test --tag pyutil:latest .
+docker build --file Dockerfile-Test --tag pyutil:test .
 
 # run a mongo container
 docker run -p 27017:27017 --name mongo -d mongo:latest
 
 # run all tests, seems to be slow on teamcity
-docker run --rm --link mongo pyutil:latest
+docker run --rm --link mongo pyutil:test
 
 ret=$?
 
 # delete the mongo container
 docker rm -f mongo
 
+docker rmi -f pyutil:test
 exit $ret
