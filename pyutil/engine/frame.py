7cca8f2f71dfca8e0067d02c5988a02d63bd253b
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-06-27 14:38:02 +0200

    Frame type

--- pyutil/engine/frame.py
+++ pyutil/engine/frame.py
@@ -1,14 +1,31 @@
 import pandas as pd
-from mongoengine import Document, StringField, DictField
+from mongoengine import Document, StringField, FileField
+
+from io import BytesIO
 
 
 class Frame(Document):
     name = StringField(required=True, max_length=200, unique=True)
-    data = DictField()
+    data = FileField()
 
     @property
     def frame(self):
-        return pd.DataFrame(self.data)
+        str_data = BytesIO(self.data.read()).read().decode()
+
+        try:
+            return pd.read_json(str_data, typ="frame")
+        except ValueError:
+            return pd.read_json(str_data, typ="series")
 
     def __str__(self):
-        return self.frame
+        return "{name}: \n{frame}".format(name=self.name, frame=self.frame)
+
+    def put(self, frame):
+        if self.data:
+            self.data.replace(frame.to_json().encode())
+        else:
+            self.data.new_file()
+            self.data.write(frame.to_json().encode())
+            self.data.close()
+
+        self.save()
--- test/test_engine/test_frame.py
+++ test/test_engine/test_frame.py
@@ -7,19 +7,26 @@ from test.config import connect
 
 import pandas.util.testing as pdt
 
+
 class TestFrame(TestCase):
     @classmethod
     def setUpClass(cls):
         connect()
-        Frame(name="f1", data=pd.DataFrame(index=["A","B"], columns=["X","Y"], data=[[2.0,3.0],[10.0,20.0]])).save()
+        f = Frame(name="f1").save()
+        f.put(frame=pd.DataFrame(index=["A","B"], columns=["X","Y"], data=[[2,3],[10,20]]))
+
+        g = Frame(name="f2").save()
+        g.put(frame=pd.Series(index=["A","B"], data=[1,2]))
 
     @classmethod
     def tearDownClass(cls):
         Frame.drop_collection()
 
     def test_count(self):
-        assert Frame.objects.count() == 1
+        assert Frame.objects.count() == 2
 
-    def test_get(self):
-        pdt.assert_frame_equal(Frame.objects(name="f1").first().frame, pd.DataFrame(index=["A","B"], columns=["X","Y"], data=[[2.0,3.0],[10.0,20.0]]))
+    def test_get_frame(self):
+        pdt.assert_frame_equal(Frame.objects(name="f1").first().frame, pd.DataFrame(index=["A","B"], columns=["X","Y"], data=[[2,3],[10,20]]))
 
+    def test_get_series(self):
+        pdt.assert_series_equal(Frame.objects(name="f2").first().frame, pd.Series(index=["A","B"], data=[1,2]))
