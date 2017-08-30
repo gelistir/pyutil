494f2c3e8739822d70f68555882ebaad31dd33c0
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-08-30 13:58:29 +0200

    dealing with portfolios

--- pyutil/sql/data.py
+++ pyutil/sql/data.py
@@ -1,11 +1,28 @@
 import pandas as pd
 from pyutil.mongo.asset import Asset
 from pyutil.mongo.assets import Assets
+from pyutil.portfolio.portfolio import Portfolio
 
 
 class Database(object):
     def __init__(self, connection):
         self.__con = connection
+
+    def __strat_id(self, strategy_name):
+        with self.__con.cursor() as cursor:
+            cursor.execute("SELECT id FROM strategy WHERE name=%(strategy)s;", {"strategy": strategy_name})
+            return cursor.fetchone()[0]
+
+    def __symbol_id(self, ticker):
+        with self.__con.cursor() as cursor:
+            cursor.execute("SELECT id FROM asset WHERE bloomberg_symbol=%(ticker)s;", {"ticker": ticker})
+            return cursor.fetchone()[0]
+
+    def __ts_id(self, ticker, field):
+        with self.__con.cursor() as cursor:
+            cursor.execute("SELECT id FROM ts_vs_asset WHERE bloomberg_symbol=%(ticker)s::integer AND ts_name=%(field)s;",
+                           {"ticker": ticker, "field": field})
+            return cursor.fetchone()[0]
 
     def __history_sql(self, assets=None, names=None):
         cond = []
@@ -56,14 +73,45 @@ class Database(object):
         else:
             return frame
 
-
     def assets(self, names):
         return Assets({name: self.asset(name) for name in names})
 
+    @property
+    def strategies(self):
+        return pd.read_sql("SELECT * FROM strategy", con=self.__con, index_col="name")
 
-if __name__ == '__main__':
-    from pyutil.sql.io import postgresql
-    db = Database(connection=postgresql(conn_str="..."))
-    asset = db.asset(name="CARMPAT FP Equity")
-    print(asset.reference)
-    print(asset.time_series)
+    def portfolio(self, name):
+        index = ["date", "bloomberg_symbol"]
+        f = lambda x: "SELECT date, bloomberg_symbol, {field} FROM strategy_data WHERE name='{strategy}'".format(strategy=name, field=x)
+
+        price = pd.read_sql(f("price"), con=self.__con, index_col=index).unstack(level=1)["price"]
+        weight = pd.read_sql(f("weight"), con=self.__con, index_col=index).unstack(level=1)["weight"]
+        return Portfolio(prices=price, weights=weight)
+
+
+    def upsert_portfolio(self, portfolio, name):
+
+        upsert = """
+                    INSERT INTO "strat_data"
+                    (date, asset_id, strategy_id, price, weight)
+                    VALUES 
+                    (%(date)s, %(asset_id)s::INTEGER, %(strategy_id)s::INTEGER, %(price)s, %(weight)s)
+                    ON CONFLICT ON CONSTRAINT time_asset_strategy
+                    DO UPDATE SET price=%(price)s, weight=%(weight)s
+                    WHERE 
+                      strat_data.strategy_id=%(strategy_id)s AND 
+                      strat_data.date=%(date)s AND 
+                      strat_data.asset_id=%(asset_id)s
+                 """
+
+        strategy_id = self.__strat_id(name)
+
+        with self.__con.cursor() as cursor:
+            for asset in portfolio.assets:
+                asset_id = self.__symbol_id(ticker=asset)
+
+                for date in portfolio.index:
+                    price = portfolio.prices.loc[date][asset]
+                    weight = portfolio.weights.loc[date][asset]
+                    data = {"date": date, "asset_id": asset_id, "strategy_id": strategy_id, "price": price, "weight": weight}
+                    cursor.execute(upsert, data)
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='2.0.01',
+    version='2.0.02',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['requests>=2.13.0', 'pandas>=0.20.0', 'pymongo', 'mongoengine']
+    description='', install_requires=['requests>=2.13.0', 'pandas>=0.20.0']
 )
