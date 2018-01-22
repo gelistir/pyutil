41b9001bf2bcd608e03073194f1eb935f3328193
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-01-22 16:02:50 +0100

    testing reports

--- test/test_sql/test_report.py
+++ test/test_sql/test_report.py
@@ -1,33 +1,34 @@
-# from unittest import TestCase
-#
-# from pyutil.sql.db import db, Strategy, Symbol, SymbolGroup
-# from pyutil.sql.pony import db_in_memory
-# from pyutil.sql.report import mtd, ytd, sector, recent, period_returns, performance
-# from test.config import test_portfolio
-#
-#
-# # class TestHistory(TestCase):
-# #
-# #     def test_strategy(self):
-# #         with db_in_memory(db):
-# #
-# #             SymbolGroup(name="A")
-# #             SymbolGroup(name="B")
-# #
-# #             for symbol in ["A", "B", "C", "D"]:
-# #                 Symbol(bloomberg_symbol=symbol, group=SymbolGroup.get(name="A"))
-# #
-# #             for symbol in ["E", "F", "G"]:
-# #                 Symbol(bloomberg_symbol=symbol, group=SymbolGroup.get(name="B"))
-# #
-# #             s = Strategy(name="test", source="No", active=True)
-# #             s.upsert_portfolio(portfolio=test_portfolio())
-# #
-# #             print(mtd())
-# #             print(mtd(names=["test"]))
-# #             print(ytd())
-# #             print(ytd(names=["test"]))
-# #             print(sector())
-# #             print(recent())
-# #             print(period_returns())
-# #             print(performance())
+from unittest import TestCase
+
+from pony import orm
+
+from pyutil.sql.db import define_database, upsert_portfolio
+from pyutil.sql.report import mtd, ytd, sector, recent, period_returns, performance
+from test.config import test_portfolio
+
+
+class TestHistory(TestCase):
+
+    def test_strategy(self):
+        db = define_database(provider='sqlite', filename=":memory:")
+
+        with orm.db_session:
+            db.SymbolGroup(name="A")
+            db.SymbolGroup(name="B")
+
+            for symbol in ["A", "B", "C", "D"]:
+                db.Symbol(bloomberg_symbol=symbol, group=db.SymbolGroup.get(name="A"))
+
+            for symbol in ["E", "F", "G"]:
+                db.Symbol(bloomberg_symbol=symbol, group=db.SymbolGroup.get(name="B"))
+
+            upsert_portfolio(db, portfolio=test_portfolio(), name="test")
+
+            print(mtd(db))
+            print(mtd(db, names=["test"]))
+            print(ytd(db))
+            print(ytd(db, names=["test"]))
+            print(sector(db))
+            print(recent(db))
+            print(period_returns(db))
+            print(performance(db))
