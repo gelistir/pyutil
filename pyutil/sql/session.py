24e79b3512a81847bd66b29e02d6282c9a368baa
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-02-21 09:31:05 +0100

    session aux

---
+++ pyutil/sql/session.py
@@ -0,0 +1,76 @@
+from contextlib import contextmanager
+
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+@contextmanager
+def session_scope(session):
+    """Provide a transactional scope around a series of operations."""
+    session = session
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+def get(session, cls, filter):
+    return session.query(cls).filter(*filter).first()
+
+def session(server=None, db=None, user=None, password=None):
+    user = user or os.environ["user"]
+    password = password or os.environ["password"]
+    server = server or os.environ["server"]
+    db = db or os.environ["db"]
+
+    engine = create_engine('postgresql+psycopg2://{user}:{password}@{server}/{db}'.format(user=user, password=password, server=server, db=db))
+    return sessionmaker(bind=engine)()
+
+
+def test_session(Base):
+    #with TestEnv() as env:
+    engine = create_engine("sqlite://")
+
+    # make the tables...
+    Base.metadata.create_all(engine)
+
+    return sessionmaker(bind=engine)()
+
+#
+# def upsert(session, cls, get, set=None):
+#     """
+#     Interacting with Pony entities.
+#
+#     :param cls: The actual entity class
+#     :param get: Identify the object (e.g. row) with this dictionary
+#     :param set:
+#     :return:
+#     """
+#     # does the object exist
+#     #assert isinstance(cls, EntityMeta), "{cls} is not a database entity".format(cls=cls)
+#
+#     # if no set dictionary has been specified
+#     set = set or {}
+#
+#     if not session.query(cls).filter(*get).first():
+#         cls(*get, *set)
+#     if not cls.exists(**get):
+#         # make new object
+#         return cls(**set, **get)
+#     else:
+#         # get the existing object
+#         obj = cls.get(**get)
+#         for key, value in set.items():
+#             obj.__setattr__(key, value)
+#         return obj
+#
+# if __name__ == '__main__':
+#
+#     ll = ["A","B"]
+#     print(ll)
+#     print(*ll)
+
--- test/test_sql/test_report.py
+++ test/test_sql/test_report.py
@@ -1,44 +1,31 @@
 from unittest import TestCase
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 from pyutil.sql.models import Base, SymbolGroup, Symbol, PortfolioSQL
 from pyutil.sql.report import mtd, ytd, sector, recent, period_returns, performance, reference, history
+from pyutil.sql.session import test_session, session_scope
 from test.config import test_portfolio
 
 
 class TestHistory(TestCase):
 
-    @staticmethod
-    def session():
-        #with TestEnv() as env:
-        engine = create_engine("sqlite://")
-
-        # make the tables...
-        Base.metadata.create_all(engine)
-
-        return sessionmaker(bind=engine)()
-
-
     def test_strategy_2(self):
-        session = self.session()
-        g1 = SymbolGroup(name="A", symbols= [Symbol(bloomberg_symbol=symbol) for symbol in ["A","B","C","D"]])
-        g2 = SymbolGroup(name="B", symbols= [Symbol(bloomberg_symbol=symbol) for symbol in ["E","F","G"]])
+        with session_scope(session=test_session(Base=Base)) as session:
+            g1 = SymbolGroup(name="A", symbols= [Symbol(bloomberg_symbol=symbol) for symbol in ["A","B","C","D"]])
+            g2 = SymbolGroup(name="B", symbols= [Symbol(bloomberg_symbol=symbol) for symbol in ["E","F","G"]])
 
-        session.add_all([g1, g2])
-        session.add(PortfolioSQL(portfolio=test_portfolio(), name="test"))
+            session.add_all([g1, g2])
+            session.add(PortfolioSQL(portfolio=test_portfolio(), name="test"))
 
-        print(mtd(session))
-        print(ytd(session))
-        print(mtd(session, names=["test"]))
-        print(ytd(session, names=["test"]))
+            print(mtd(session))
+            print(ytd(session))
+            print(mtd(session, names=["test"]))
+            print(ytd(session, names=["test"]))
 
-        print(sector(session))
-        print(recent(session))
-        print(period_returns(session))
-        print(performance(session))
-        print(reference(session))
-        print(history(session))
-        #assert False
+            print(sector(session))
+            print(recent(session))
+            print(period_returns(session))
+            print(performance(session))
+            print(reference(session))
+            print(history(session))
+
 
