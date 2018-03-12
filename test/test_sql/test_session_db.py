ee3ed69a35a1ca71fff28a92c0b4783544d9ce1e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-03-12 17:57:17 +0100

    testing models

--- test/test_sql/test_session_db.py
+++ test/test_sql/test_session_db.py
@@ -1,29 +1,73 @@
 from unittest import TestCase
 
-from pyutil.sql.models import Base, Symbol, PortfolioSQL, SymbolType
+import pandas as pd
+
+from pyutil.sql.aux import asset, reference, history
+from pyutil.sql.models import Base, Symbol, PortfolioSQL, SymbolType, Field, Timeseries
 from pyutil.sql.session import session_test, session_scope, SessionDB
+
+import pandas.util.testing as pdt
 
 
 class TestHistory(TestCase):
+    def test_reference(self):
+        session = session_test(meta=Base.metadata)
+        s1 = Symbol(bloomberg_symbol="A")
+        session.add(s1)
 
-    def test_strategy_2(self):
-        with session_scope(session=session_test(meta=Base.metadata)) as session:
-            db = SessionDB(session=session)
+        f1 = Field(name="Field 1")
+        f2 = Field(name="Field 2")
+        session.add_all([f1, f2])
 
-            #x = db.upsert_one(SymbolGroup, get={"name": "A"})
-            #y = db.upsert_one(SymbolGroup, get={"name": "A"})
-            #z = db.upsert_one(SymbolGroup, get={"name": "B"})
+        with session.no_autoflush:
+             s1.update_reference(f1, "100")
+             s1.update_reference(f2, "200")
 
-            #self.assertEqual(x, y)
+        f = pd.DataFrame(columns=["Field 1", "Field 2"], index=["A"], data=[["100", "200"]])
+        f.index.name = "Asset"
 
-            s1 = db.upsert_one(Symbol, get={"bloomberg_symbol": "A"})
-            s2 = db.upsert_one(Symbol, get={"bloomberg_symbol": "A"})
+        pdt.assert_frame_equal(f, reference(session))
 
-            self.assertEqual(s1, s2)
+    def test_asset(self):
+        session = session_test(meta=Base.metadata)
+        s1 = Symbol(bloomberg_symbol="A")
+        session.add(s1)
+        self.assertEqual(s1, asset(session, name="A"))
+
+    def test_history(self):
+        session = session_test(meta=Base.metadata)
+
+        s1 = Symbol(bloomberg_symbol="C")
+        session.add(s1)
+
+        x = pd.Series({pd.Timestamp("2010-01-01").date(): 100.0, pd.Timestamp("2011-01-01").date(): 200.0})
+        s1.timeseries["PX_LAST"] = Timeseries(name="PX_LAST").upsert(ts=x)
 
 
-            p = db.upsert_one(PortfolioSQL, get={"name": "Peter Maffay"})
-            self.assertTrue(p.empty)
+        pdt.assert_series_equal(s1.timeseries["PX_LAST"].series, x)
 
-            f = db.upsert_one(Symbol, get={"bloomberg_symbol": "A"}, set={"group": SymbolType.equities})
-            self.assertEqual(f.group.name, "equities")
+        g = x.to_frame("C")
+        g.index.name = "Date"
+        g.columns.name = "Asset"
+
+        # this is weird...
+        g.index = pd.to_datetime(g.index)
+
+        pdt.assert_frame_equal(history(session), g)
+
+
+
+
+    def test_portfolio(self):
+        session = session_test(meta=Base.metadata)
+
+        p = self.db.upsert_one(PortfolioSQL, get={"name": "Peter Maffay"})
+        self.assertTrue(p.empty)
+
+        f = self.db.upsert_one(Symbol, get={"bloomberg_symbol": "A"}, set={"group": SymbolType.equities})
+        self.assertEqual(f.group.name, "equities")
+
+        a = asset(session=self.session, name="A")
+
+        self.assertEqual(s1, a)
+
