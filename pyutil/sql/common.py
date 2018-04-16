4acfc8df7dd71b82425ec34c06f28d708a031087
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-04-16 16:28:38 +0200

    making productInterface more robust

--- pyutil/sql/common.py
+++ pyutil/sql/common.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 # todo: test this
 def reference(products):
-    x = pd.DataFrame({product: product.reference for product in products}).transpose()
+    x = pd.DataFrame({product: product.reference.to_pandas() for product in products}).transpose()
     x.index.name = "Product"
     return x
 
 
 def history(products, field="PX_LAST"):
     # note you can use this
-    x = pd.DataFrame({product: product.get_pandas(names=field) for product in products})
+    x = pd.DataFrame({product: product.timeseries[field] for product in products})
     # or this
     # x = pd.DataFrame({product: product.timeseries[field] for product in products})
     # the second option will crash as there is no timeseries field for the product
