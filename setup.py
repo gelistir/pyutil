d6f94f0407740d51183dac5c0f370f79f7c04a93
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-05-24 14:44:11 +0200

    html to dict

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='3.5.6',
+    version='3.5.7',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
