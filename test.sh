85259edb711b0197845edec24a7cc26c3678d8ec
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-06-01 10:45:22 +0200

    setup running postgres server in tests

--- test.sh
+++ test.sh
@@ -4,6 +4,15 @@ docker build --file Dockerfile --no-cache --target test --tag pyutil:test .
 # delete all files in html-coverage
 rm -rf $(pwd)/html-coverage/*
 
+# step 1: create a postgresql server running on server "test-postgresql" with user "postgres"
+#         and database "postgres", password is "test"
+docker run --name test-postgresql -e POSTGRES_PASSWORD=test -d postgres:9.6
+
+# step 2: wait for postgres to be up and running...
+docker run --link test-postgresql pyutil:test /pyutil/wait-for-postgres.sh test-postgresql
+
+
+
 # run all tests, seems to be slow on teamcity
 docker run --rm -v $(pwd)/html-coverage:/html-coverage  -v $(pwd)/html-report:/html-report pyutil:test
 
