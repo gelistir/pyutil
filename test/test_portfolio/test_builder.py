d02023a0bddf8495d7bef6fe325a8e318e0b5d0b
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-06-15 11:51:55 +0200

    test merge

--- test/test_portfolio/test_builder.py
+++ test/test_portfolio/test_builder.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from unittest import TestCase
 
-from pyutil.portfolio.portfolio import Portfolio
+from pyutil.portfolio.portfolio import Portfolio, merge
 
 
 class TestPortfolioBuilder(TestCase):
@@ -38,3 +38,32 @@ class TestPortfolioBuilder(TestCase):
         portfolio = Portfolio(prices = pd.DataFrame({}))
         self.assertIsNone(portfolio.last_valid_index)
         self.assertTrue(portfolio.empty)
+
+    def test_merge(self):
+        prices1 = pd.DataFrame(columns=["B", "A"], index=[1, 2], data=100)
+
+        portfolio1 = Portfolio(prices=prices1)
+        portfolio1.weights.loc[1] = {"A": 0.5, "B": 0.5}
+        portfolio1.weights.loc[2] = {"A": 0.3, "B": 0.7}
+
+        prices2 = pd.DataFrame(columns=["C", "D"], index=[1, 2], data=200)
+
+        portfolio2 = Portfolio(prices=prices2)
+        portfolio2.weights.loc[1] = {"C": 0.5, "D": 0.5}
+        portfolio2.weights.loc[2] = {"C": 0.3, "D": 0.7}
+
+        portfolio = merge(portfolios=[portfolio1, portfolio2], axis=1)
+
+        self.assertListEqual(portfolio.assets, ["A","B","C","D"])
+
+
+        prices3 = pd.DataFrame(columns=["A", "B"], index=[1, 2], data=200)
+
+        portfolio3 = Portfolio(prices=prices3)
+        portfolio3.weights.loc[1] = {"A": 0.5, "B": 0.5}
+        portfolio3.weights.loc[2] = {"A": 0.3, "B": 0.7}
+
+        with self.assertRaises(ValueError):
+            # overlapping columns!
+            merge(portfolios=[portfolio1, portfolio3], axis=1)
+
