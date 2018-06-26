70c1ac9d8030e38ac3563c48b8d078e4c11622ed
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-06-26 17:38:15 +0200

    influx tests

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='3.5.13',
+    version='3.6.0',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['requests>=2.13.0', 'pandas>=0.21.0', 'sqlalchemy']
+    description='', install_requires=['requests>=2.13.0', 'pandas>=0.21.0', 'sqlalchemy', 'influxdb']
 )
