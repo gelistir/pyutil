2f3b4ae7cb4ec3cf1e5700fb1a401c1024611225
tschm <thomas.schmelzer@gmail.com>
2018-07-10 20:03:13 +0200

    cleaning source

--- test/resources/source.py
+++ test/resources/source.py
@@ -1,12 +1,10 @@
-import pandas as pd
-
 from test.config import test_portfolio
 
 
 class Configuration(object):
     def __init__(self, reader):
         self.__reader = reader
-        self.__portfolio = test_portfolio()#.truncate(before=pd.Timestamp("2015-04-01"))
+        self.__portfolio = test_portfolio()
 
     @property
     def portfolio(self):
