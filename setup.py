8cee454c5c80825bd4d840512150b93808b8fe45
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-07-12 15:13:26 +0200

    remove the addepar and symbols views

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['requests>=2.13.0', 'pandas>=0.23.1', 'sqlalchemy', 'influxdb']
+    description='', install_requires=['requests>=2.19.0', 'pandas>=0.23.1', 'sqlalchemy', 'influxdb']
 )
