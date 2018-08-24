034225cfdc61fb9883704018e728daf20f9b8cd0
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-24 16:21:13 +0200

    pyan

---
+++ graph.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker-compose -f docker-compose.test.yml run test-pyutil python pyan.py pyutil/**/*.py -v --uses --defines --colored --dot --nested-groups > graph/architecture.dot
+docker-compose -f docker-compose.test.yml run test-pytuil dot -Tsvg graph/architecture.dot > graph/architecture.svg
+
