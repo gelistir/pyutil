43dfa55ad1e11a76c518b8089dff6933782fa482
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-28 15:07:48 +0200

    readonly

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose.test.yml build
+./graph.sh
 docker-compose -f docker-compose.test.yml run test-pyutil
 ./sphinx.sh
-./graph.sh
+
+# remove all containers that are exited...
+docker rm $(docker ps -q -f status=exited)
+# remove the volumes hanging around...
+docker volume prune --force
