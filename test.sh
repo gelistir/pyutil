98a305b172756fc7dfba8bfe0c1dd1fdb5c7096b
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-29 12:23:39 +0200

    readonly

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose.test.yml build test
-./graph.sh
 docker-compose -f docker-compose.test.yml run test
-./sphinx.sh
+
+ret=$?
 
 # removes also all the containers linked to this particular service "test" defined in "docker-compose.test.yml"
 docker-compose -f docker-compose.test.yml rm -v -f test
+
+exit $ret
