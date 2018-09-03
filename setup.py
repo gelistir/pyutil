f5ae4796a52aa23e41a7932e8759049bef0d6d12
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-09-03 16:45:38 +0200

    ytd Series with Monthnames

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyutil',
-    version='3.7.3',
+    version='3.7.4',
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
