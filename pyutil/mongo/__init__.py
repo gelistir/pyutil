b4d6f860c9a8884fd308297a1e8a94c85937ccbb
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-10-18 16:15:20 +0200

    timeseries in sql

--- pyutil/mongo/__init__.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class MClient(MongoClient):
-    def __init__(self, host, port=27017, **kwargs):
-        super().__init__(host, port, **kwargs)
-
-
-    #def list_database_names(self, session=None):
-
