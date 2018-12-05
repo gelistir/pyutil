44c6a3fb5afebd4f5cd99a7dda1e7739d3c17988
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-12-05 14:00:32 +0100

    to_datetime and simplifying update history

--- pyutil/quant/history.py
+++ pyutil/quant/history.py
@@ -8,9 +8,9 @@ def update_history(symbol, reader, t0=pd.Timestamp("2000-01-01"), offset=10):
     t = last_index(symbol.price, default=t0 + offset) - offset
 
     # merge new data with old existing data if it exists
-    series = reader(tickers=symbol.name, t0=t).dropna()
-    assert isinstance(series, pd.Series)
-
-    if not series.empty:
-       return symbol.upsert_price(ts=to_datetime(series))
+    series = reader(tickers=symbol.name, t0=t)#.dropna()
+    #assert isinstance(series, pd.Series)
+    if series is not None:
+    #if not series.empty:
+       return symbol.upsert_price(ts=to_datetime(series.dropna()))
 
