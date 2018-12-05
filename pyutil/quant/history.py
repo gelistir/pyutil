e7bc037fe9213d0f83eec695be34f6ba72754e5f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-12-05 12:26:18 +0100

    to_datetime and simplifying update history

--- pyutil/quant/history.py
+++ pyutil/quant/history.py
@@ -1,19 +1,12 @@
-import logging
 import pandas as pd
 from pyutil.timeseries.merge import last_index, to_datetime
 
 
-def update_history(symbol, reader, t0=pd.Timestamp("2000-01-01"), offset=10, logger=None):
-    logger = logger or logging.getLogger(__name__)
-
+def update_history(symbol, reader, t0=pd.Timestamp("2000-01-01"), offset=10):
     offset = pd.offsets.Day(n=offset)
 
     t = last_index(symbol.price, default=t0 + offset) - offset
 
-    try:
-        # merge new data with old existing data if it exists
-        return symbol.upsert_price(ts=to_datetime(reader(tickers=symbol.name, t0=t).dropna()))
+    # merge new data with old existing data if it exists
+    return symbol.upsert_price(ts=to_datetime(reader(tickers=symbol.name, t0=t).dropna()))
 
-    except Exception as e:
-        logger.warning("Problem {e} with Ticker: {ticker}".format(e=e, ticker=symbol.name))
-        pass
