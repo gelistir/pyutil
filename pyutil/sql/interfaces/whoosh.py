334248459dd1d49f06c82e9ac254344f26f53bf9
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-01-16 11:24:31 +0100

    whoosh

--- pyutil/sql/interfaces/whoosh.py
+++ pyutil/sql/interfaces/whoosh.py
@@ -1,4 +1,6 @@
 import datetime
+import pandas as pd
+
 import sqlalchemy as sq
 from pyutil.sql.base import Base
 
@@ -7,12 +9,20 @@ class Whoosh(Base):
     __tablename__ = "whoosh"
     __searchable__ = ['title', 'content']  # these fields will be indexed by whoosh
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.Text)
-    content = sq.Column(sq.Text)
-    path = sq.Column(sq.Text)
-    group = sq.Column(sq.Text)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True, nullable=False)
+    title = sq.Column(sq.Text, nullable=False)
+    content = sq.Column(sq.Text, nullable=False)
+    path = sq.Column(sq.Text, nullable=False)
+    group = sq.Column(sq.Text, nullable=False)
     created = sq.Column(sq.DateTime, default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return '{0}(title={1})'.format(self.__class__.__name__, self.title)
+
+    @staticmethod
+    def frame(rows):
+        frame = pd.DataFrame({row.id: {"group": row.group, "path": row.path, "content": row.content, "title": row.title, "created": row.created} for row in rows}).transpose()
+        frame.index.name = "Symbol"
+        frame = frame.sort_index()
+        return frame
+
