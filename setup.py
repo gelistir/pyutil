4e8d3056d718ecae6985279d4a0f84d0639c0af4
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-02-04 13:51:35 +0100

    dependency psycopg2

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages(include=["pyutil*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['requests>=2.19.0', 'pandas>=0.23.1', 'sqlalchemy', 'psycopg2=2.7.5']
+    description='', install_requires=['requests>=2.19.0', 'pandas>=0.23.1', 'sqlalchemy', 'psycopg2==2.7.5']
 )
