1b9a5a56822b0c7606a2346668a8aaaecdcddb7a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-02-21 14:20:35 +0100

    jupyter support

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 docker-compose build pyutil
+docker-compose build jupyter
 
 
---
+++ jupyter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "http://localhost:9020"
+docker-compose run -p "9020:8888" jupyter
