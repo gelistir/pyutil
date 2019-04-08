7ec703142361d64d4847ba2bd4e2196c8ebb0d48
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-04-08 10:49:07 +0200

    make test with travis

--- pyutil/performance/month.py
+++ pyutil/performance/month.py
@@ -2,6 +2,9 @@ import calendar
 import numpy as np
 
 
+def _cumulate(x):
+    return (1 + x).prod() - 1.0
+
 def monthlytable(nav):
     """
     Get a table of monthly returns
@@ -13,12 +16,13 @@ def monthlytable(nav):
     r = nav.pct_change().dropna()
     # Works better in the first month
     # Compute all the intramonth-returns, instead of reapplying some monthly resampling of the NAV
-    return_monthly = r.groupby([lambda x: x.year, lambda x: x.month]).apply(lambda x: (1 + x).prod() - 1.0)
+    return_monthly = r.groupby([r.index.year, r.index.month]).apply(_cumulate)
     frame = return_monthly.unstack(level=1).rename(columns=lambda x: calendar.month_abbr[x])
-    a = (frame + 1.0).prod(axis=1) - 1.0
+    ytd = frame.apply(_cumulate, axis=1)
     frame["STDev"] = np.sqrt(12) * frame.std(axis=1)
     # make sure that you don't include the column for the STDev in your computation
-    frame["YTD"] = a
+    frame["YTD"] = ytd
     frame.index.name = "Year"
+    frame.columns.name = None
     # most recent years on top
     return frame.iloc[::-1]
