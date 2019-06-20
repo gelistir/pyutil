3f367e36718fad313aec07610e281107d785627a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-06-20 11:13:16 +0200

    Mongo support

--- pyutil/mongo/mongo.py
+++ pyutil/mongo/mongo.py
@@ -1,64 +1,60 @@
 import os
+import random
+import string
+
 import pandas as pd
 from pymongo import MongoClient
 
 _mongo = MongoClient(host=os.environ["MONGO_HOST"], port=27017)[os.environ["MONGO_DATABASE"]]
 
 
-def collection(name):
-    return _Collection(_mongo[name])
+def collection(name=None, write=True):
+    name = name or "".join(random.choices(string.ascii_lowercase, k=10))
+    return _Collection(_mongo[name], write=write)
 
 
 class _Collection(object):
-    def __init__(self, collection):
+    def __init__(self, collection, write=False):
         self.__collection = collection
+        self.__write = write
+
+    @property
+    def write(self):
+        return self.__write
+
+    @write.setter
+    def write(self, value):
+        self.__write = value
 
     @property
     def name(self):
         return self.collection.name
 
     def upsert(self, p_obj, **kwargs):
+        assert self.write, "It is forbidden to write into this collection"
+
         # check it's either unique or not there
-        n = self.__collection.count_documents({**kwargs})
+        assert self.__collection.count_documents({**kwargs}) <= 1, "Identifier not unique"
 
-        if n == 0:
-            self.__collection.insert_one({"data": p_obj.to_msgpack(), **kwargs})
-            return p_obj
-
-        if n == 1:
-            self.__collection.update_one({**kwargs}, {"$set": {"data": p_obj.to_msgpack()}}, upsert=False)
-            return p_obj
-
-        if n > 1:
-            assert False, "Identifier not unique"
+        self.__collection.update_one({**kwargs}, {"$set": {"data": p_obj.to_msgpack()}}, upsert=True)
+        return p_obj
 
     def find(self, parse=False, **kwargs):
         for a in self.__collection.find({**kwargs}):
-            if parse:
-                yield _Collection.parse(a)
-            else:
-                yield a
+            yield self.__parse(a, parse=parse)
+
+    def __parse(self, a, parse=True):
+        if parse:
+            try:
+                return pd.read_msgpack(a["data"])
+            except TypeError:
+                return None
+        else:
+            return a
 
     def find_one(self, parse=False, **kwargs):
-        n = self.__collection.count_documents({**kwargs})
-        assert n <= 1, "Found multiple {n} documents".format(n=n)
-        if n == 0:
-            return None
-
-        a = self.__collection.find_one({**kwargs})
-
-        # you may want to parse
-        if parse:
-            a = _Collection.parse(a)
-
-        return a
-
-    @staticmethod
-    def parse(x=None):
-        try:
-            return pd.read_msgpack(x["data"])
-        except TypeError:
-            return None
+        assert self.__collection.count_documents({**kwargs}) <= 1, "Found multiple documents."
+        return self.__parse(self.__collection.find_one({**kwargs}), parse)
 
     @property
     def collection(self):
@@ -68,4 +64,4 @@ class _Collection(object):
         return self.__collection.__repr__()
 
     def frame(self, key, **kwargs):
-        return pd.DataFrame({x[key]: _Collection.parse(x) for x in self.find(**kwargs)})
+        return pd.DataFrame({x[key]: self.__parse(x) for x in self.find(**kwargs)})
--- test/test_mongo/test_mongo.py
+++ test/test_mongo/test_mongo.py
@@ -3,7 +3,7 @@ import pytest
 import pandas as pd
 import pandas.util.testing as pdt
 
-from pyutil.mongo.mongo import collection as create_collection, _Collection, _mongo
+from pyutil.mongo.mongo import collection as create_collection, _mongo
 
 
 @pytest.fixture()
@@ -15,24 +15,22 @@ def ts1():
 def ts2():
     return pd.Series(data=[300, 300], index=[1, 2])
 
+
 @pytest.fixture()
 def col(ts1):
-    collection = create_collection(name="test")
+    collection = create_collection(write=True)
     collection.upsert(p_obj=ts1, First="Hans", Last="Dampf")
     collection.upsert(p_obj=ts1, First="Hans", Last="Maffay")
+    # It is now forbidden to write into this collection
+    collection.write = False
     return collection
+
 
 class TestMongo(object):
     def test_mongo(self):
         assert _mongo
 
-    def test_parse_none(self):
-        assert not _Collection.parse(None)
-
     def test_find_one(self, col, ts1):
-        #collection.upsert(p_obj=ts1, First="Hans", Last="Dampf")
-        #collection.upsert(p_obj=ts1, First="Hans", Last="Maffay")
-
         x = col.find_one(parse=True, First="Hans", Last="Dampf")
         pdt.assert_series_equal(ts1, x)
 
@@ -41,25 +39,31 @@ class TestMongo(object):
             col.find_one(First="Hans")
 
         # there is no Peter
-        assert col.find_one(First="Peter") is None
+        assert col.find_one(parse=False, First="Peter") is None
         assert col.find_one(parse=True, First="Peter") is None
 
     def test_assert_insert(self, col, ts1):
-        #collection.upsert(p_obj=ts1, First="Hans", Last="Dampf")
-        #collection.upsert(p_obj=ts1, First="Hans", Last="Maffay")
         with pytest.raises(AssertionError):
             col.upsert(p_obj=ts1, First="Hans")  # I can not update as this is not unique
 
     def test_find(self, col):
-        #collection.upsert(p_obj=ts1, First="Hans", Last="Dampf")
-        #collection.upsert(p_obj=ts1, First="Hans", Last="Maffay")
         a = [x for x in col.find(First="Hans")]
         assert len(a) == 2
 
-    def test_name(self, col):
-        assert col.name == "test"
-
-    def test_merge(self, col, ts1, ts2):
+    def test_merge(self, ts1, ts2):
+        col = create_collection()
         col.upsert(ts1, name="x")
         col.upsert(ts2, name="x")
         pdt.assert_series_equal(ts2, col.find_one(parse=True, name="x"))
+
+    def test_upsert(self, ts1):
+        col = create_collection()
+        col.upsert(ts1, name="maffay", kind="wurst")
+        y = col.find_one(parse=False, kind="wurst")
+        assert y["name"] == "maffay"
+        assert y["kind"] == "wurst"
+        assert y["data"] == ts1.to_msgpack()
+
+    def test_frame(self, col, ts1):
+        frame = col.frame(key="Last")
+        pdt.assert_frame_equal(frame, pd.DataFrame({"Maffay": ts1, "Dampf": ts1})[frame.keys()])
