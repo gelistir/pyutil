3c419e4116eb87f83d329de3fad57b71d865bd2a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-06-25 16:28:33 +0200

    code coverage

--- pyutil/timeseries/signal.py
+++ pyutil/timeseries/signal.py
@@ -1,5 +1,4 @@
 import numpy as np
-# import warnings
 
 
 def volatility_adjust(prices, vola=32, min_periods=50, winsor=4.2, n=1):
@@ -23,27 +22,6 @@ def volatility_adjust(prices, vola=32, min_periods=50, winsor=4.2, n=1):
     return prices
 
 
-# #todo: Delete this function
-# def __volatility_adjust(prices, com=32, min_periods=50):
-#     warnings.warn("deprecated", DeprecationWarning)
-#     returns = prices.pct_change()
-#     volatility = returns.ewm(com=com, min_periods=min_periods).std(bias=False)
-#     return returns / volatility
-#
-#
-# #todo: Delete this function
-# def __winsorize(data, winsor=4.2):
-#     warnings.warn("deprecated", DeprecationWarning)
-#     return data.apply(np.clip, a_min=-winsor, a_max=winsor)
-#
-#
-# #todo: Delete this function
-# def adjprice(prices, vola=32, winsor=4.2, min_periods=50):
-#     warnings.warn("deprecated", DeprecationWarning)
-#     # volatility adjusted returns
-#     return __winsorize(__volatility_adjust(prices, vola, min_periods), winsor=winsor).cumsum()
-
-
 def oscillator(price, a=32, b=96, min_periods=100):
     def __geom(q):
         return 1.0 / (1 - q)
@@ -52,12 +30,6 @@ def oscillator(price, a=32, b=96, min_periods=100):
     l_fast = 1.0 - 1.0 / a
     l_slow = 1.0 - 1.0 / b
     return osc / np.sqrt(__geom(l_fast**2) - 2.0 * __geom(l_slow * l_fast) + __geom(l_slow**2))
-
-
-# #todo: Delete this function
-# def trend(price, a=32, b=96, vola=32, winsor=4.2, min_periods=50, f=np.tanh):
-#     warnings.warn("deprecated", DeprecationWarning)
-#     return f(oscillator(adjprice(price, vola, winsor, min_periods), a, b, 2 * min_periods))
 
 
 def trend_new(price, a=32, b=96, vola=32, winsor=4.2, min_periods=50, f=np.tanh, n=1):
