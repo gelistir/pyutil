2136d7d93de99b11874a8140a389485e87917d87
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-07-25 10:19:59 +0200

    Analysis taking frame instead of portfolios

--- pyutil/portfolio/analysis.py
+++ pyutil/portfolio/analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from pyutil.performance.summary import NavSeries
+
 
 def __last(frame, datefmt=None):
     frame = frame.sort_index(axis=1, ascending=False)
@@ -10,79 +12,40 @@ def __last(frame, datefmt=None):
     return frame
 
 
-def nav(portfolios, f=lambda x: x.nav) -> pd.DataFrame:
+def nav(portfolios) -> pd.DataFrame:
     assert isinstance(portfolios, dict)
-    frame = pd.DataFrame({name: f(portfolio) for name, portfolio in portfolios.items()})
+    frame = pd.DataFrame({name: portfolio.nav for name, portfolio in portfolios.items()})
     frame.columns.name = "Portfolio"
     return frame
 
-def mtd(portfolios):
-    frame = nav(portfolios=portfolios, f=lambda x: x.nav.mtd_series).transpose()
-    return __last(frame, datefmt="%b %d")
+
+def mtd(frame) -> pd.DataFrame:
+    x = frame.apply(lambda x: NavSeries(x).mtd_series).transpose()
+    return __last(x, datefmt="%b %d")
 
 
-def ytd(portfolios):
-    frame = nav(portfolios=portfolios, f=lambda x: x.nav.ytd_series).transpose()
-    return __last(frame, datefmt="%m")
+def ytd(frame) -> pd.DataFrame:
+    x = frame.apply(lambda x: NavSeries(x).ytd_series).transpose()
+    return __last(x, datefmt="%m")
 
 
-def recent(portfolios, n=15):
-    # define the function
-    frame = nav(portfolios=portfolios, f=lambda x: x.nav.recent(n)).tail(n).transpose()
-    return __last(frame, datefmt="%b %d")
+def recent(frame, n=15) -> pd.DataFrame:
+    x = frame.apply(lambda x: NavSeries(x).recent(n=n)).tail(n).transpose()
+    return __last(x, datefmt="%b %d")
 
 
-def sector(portfolios, symbolmap, total=False):
+def sector(portfolios, symbolmap, total=False) -> pd.DataFrame:
     assert isinstance(portfolios, dict)
-    return nav(portfolios=portfolios, f=lambda x: x.sector(symbolmap, total=total).iloc[-1])
-
-    #frame = pd.DataFrame(
-    #    {name: portfolio.sector(symbolmap, total=total).iloc[-1] for name, portfolio in portfolios.items()}).transpose()
-    #frame.index.name = "Portfolio"
-    #return frame
+    return pd.DataFrame({name : portfolio.sector(symbolmap=symbolmap, total=total).iloc[-1] for name, portfolio in portfolios.items()})
 
 
-def performance(portfolios, **kwargs):
-    return nav(portfolios=portfolios, f=lambda x: x.nav.summary_format(**kwargs))
+def performance(frame, **kwargs) -> pd.DataFrame:
+    return frame.apply(lambda x: NavSeries(x).summary_format(**kwargs))
 
 
-def drawdown(portfolios):
-    return nav(portfolios=portfolios, f=lambda x: x.nav.drawdown)
+def drawdown(frame) -> pd.DataFrame:
+    return frame.apply(lambda x: NavSeries(x).drawdown)
 
 
-def ewm_volatility(portfolios, **kwargs):
-    return nav(portfolios=portfolios, f=lambda x: x.nav.ewm_volatility(**kwargs))
-
-
-def period(portfolios, before=None, after=None, adjust=True):
-    return nav(portfolios=portfolios, f=lambda x: x.nav.truncate(before=before, after=after, adjust=adjust))
-
-
-def monthlytable(portfolios):
-    assert isinstance(portfolios, dict)
-    return {name: portfolio.nav.monthlytable for name, portfolio in portfolios.items()}
-
-def drawdown_periods(portfolios):
-    assert isinstance(portfolios, dict)
-    return {name: portfolio.nav.drawdown_periods for name, portfolio in portfolios.items()}
-    #nav_period = nav.truncate(before=start, after=end).apply(adjust)
-    #o.addItem(display_performance(nav_period), "Performance")
-    #plot_entire = SimpleTimePlot(nav, nav.keys())
-
-    #if start and end:
-    #    plot_entire.add(ConstantBand(x=[start, end]))
-    #else:
-    #    if days:
-    #        for day in days:
-    #            plot_entire.add(ConstantLine(x=day))
-
-    #o.addItem(plot_entire, "History")
-
-    #if start and end:
-    #    plot = SimpleTimePlot(nav_period, nav_period.keys())  # , "Period")
-    #    if days:
-    #        for day in days:
-    #            plot.add(ConstantLine(x=day))
-    #    o.addItem(plot, "Period")
-
-    #return o
+def ewm_volatility(frame, **kwargs) -> pd.DataFrame:
+    return frame.apply(lambda x: NavSeries(x).ewm_volatility(**kwargs))
--- test/test_portfolio/test_analysis.py
+++ test/test_portfolio/test_analysis.py
@@ -24,40 +24,28 @@ class TestAnalysis(object):
         pdt.assert_series_equal(frame["test"], portfolio.nav.series, check_names=False)
 
     def test_drawdown(self, portfolio, portfolios):
-        frame = ppa.drawdown(portfolios)
+        frame = ppa.drawdown(ppa.nav(portfolios))
         pdt.assert_series_equal(frame["test"], portfolio.nav.drawdown, check_names=False)
 
     def test_mtd(self, portfolio, portfolios):
-        frame = ppa.mtd(portfolios)
-        assert frame.loc["test"]["Apr 20"] == portfolio.nav.mtd_series[pd.Timestamp("2015-04-20")]
+        xxx = ppa.mtd(frame=ppa.nav(portfolios))
+        assert xxx.loc["test"]["Apr 20"] == portfolio.nav.mtd_series[pd.Timestamp("2015-04-20")]
 
     def test_ytd(self, portfolio, portfolios):
-        frame = ppa.ytd(portfolios)
-        assert frame.loc["test"]["03"] == portfolio.nav.ytd_series[pd.Timestamp("2015-03-31")]
+        xxx = ppa.ytd(frame=ppa.nav(portfolios))
+        assert xxx.loc["test"]["03"] == portfolio.nav.ytd_series[pd.Timestamp("2015-03-31")]
 
     def test_recent(self, portfolio, portfolios):
-        frame = ppa.recent(portfolios)
-        assert frame.loc["test"]["Apr 20"] == portfolio.nav.recent(n=20)[pd.Timestamp("2015-04-20")]
+        xxx = ppa.recent(ppa.nav(portfolios))
+        assert xxx.loc["test"]["Apr 20"] == portfolio.nav.recent(n=20)[pd.Timestamp("2015-04-20")]
 
     def test_performance(self, portfolios):
-        frame = ppa.performance(portfolios)
+        frame = ppa.performance(ppa.nav(portfolios))
         assert frame["test"]["Kurtosis"] == "6.98"
 
     def test_ewm_volatility(self, portfolio, portfolios):
-        frame = ppa.ewm_volatility(portfolios)
+        frame = ppa.ewm_volatility(ppa.nav(portfolios))
         pdt.assert_series_equal(frame["test"], portfolio.nav.ewm_volatility(), check_names=False)
-
-    def test_period(self, portfolio, portfolios):
-        frame = ppa.period(portfolios, before=pd.Timestamp("2015-01-01"))
-        pdt.assert_series_equal(frame["test"], portfolio.nav.truncate(before=pd.Timestamp("2015-01-01"), adjust=True).series, check_names=False)
-
-    def test_monthlytable(self, portfolio, portfolios):
-        x = ppa.monthlytable(portfolios=portfolios)
-        pdt.assert_frame_equal(x["test"].applymap(percentage), portfolio.nav.monthlytable.applymap(percentage))
-
-    def test_drawdown_periods(self, portfolio, portfolios):
-        x = ppa.drawdown_periods(portfolios=portfolios)
-        pdt.assert_series_equal(x["test"], portfolio.nav.drawdown_periods)
 
     def test_sector(self, portfolio, portfolios):
         symbolmap = {"A": "A", "B": "A", "C": "B", "D": "B", "E": "C", "F": "C", "G": "C"}
