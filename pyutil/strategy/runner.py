197307c6e376f422329a27a0f0d7eb26b1dd3af2
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-08-22 15:37:45 +0200

    testing

--- pyutil/strategy/runner.py
+++ pyutil/strategy/runner.py
@@ -2,10 +2,6 @@ import logging
 from functools import partial
 import multiprocessing as mp
 
-import pandas as pd
-
-from pyutil.portfolio.portfolio import Portfolio
-
 from pyutil.sql.interfaces.symbols.strategy import Strategy
 from pyutil.sql.interfaces.symbols.symbol import Symbol
 
@@ -24,22 +20,9 @@ def _strategy_update(strategy_id, connection_str, logger=None):
         Symbol.refresh_mongo()
 
         strategy = session.query(Strategy).filter_by(id=strategy_id).one()
-
-        #last = strategy.last_valid_index
-
         logger.debug(strategy.name)
-        #logger.debug(last)
 
-        portfolio_new = strategy.configuration(reader=reader(session)).portfolio
-
-        #if last is not None:
-            # use only the very last few days...
-            #portfolio_new = portfolio_new.truncate(before=last - pd.DateOffset(days=n))
-            #strategy.portfolio = Portfolio.merge(new=portfolio_new, old=strategy.portfolio)
-        #else:
-            #strategy.portfolio = portfolio_new
-
-        return strategy.name, portfolio_new
+        return strategy.name, strategy.configuration(reader=reader(session)).portfolio
 
 
 def run(strategies, connection_str, logger=None):
