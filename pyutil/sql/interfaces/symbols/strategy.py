3934c6194cd6253f06b47a5ed06d6e5780bb574f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-10-09 13:16:41 +0200

    testing

--- pyutil/sql/interfaces/symbols/strategy.py
+++ pyutil/sql/interfaces/symbols/strategy.py
@@ -85,6 +85,3 @@ class Strategy(Product, Base):
     @classmethod
     def navs(cls, strategies, f=lambda x: x) -> pd.DataFrame:
         return pd.DataFrame({f(strategy): strategy.portfolio.nav for strategy in strategies})
-        #frame.columns = = frame.rename(columns=map(f, frame.keys()))
-        #return frame
-
