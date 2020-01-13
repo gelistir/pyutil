3277a43a5edb873da891e1a4d93b946f4f0f5de9
Thomas Schmelzer <thomas.schmlezer@gmail.com>
2020-01-13 21:47:13 +0100

    util remove

--- pyutil/config/util.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-import pandas as pd
-
-
-def folder_stamped(*args):
-    folder = os.path.join(os.path.join(*args), "{t}".format(t=pd.Timestamp("today").strftime("%Y%m%d")))
-    pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
-    return folder
