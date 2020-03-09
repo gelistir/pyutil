2bd84e4414600a0b63f2d62492a3ad56dd1de363
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-03-09 12:07:51 +0100

    stamped folder

--- pyutil/config/folder.py
+++ pyutil/config/folder.py
@@ -3,8 +3,8 @@ import pathlib
 import pandas as pd
 
 
-def folder_stamped(date=None, *args):
-    date = date or pd.Timestamp("today")
+def folder_stamped(*args):
+    date = pd.Timestamp("today")
     folder = os.path.join(os.path.join(*args), "{t}".format(t=date.strftime("%Y%m%d")))
     pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
     return folder
